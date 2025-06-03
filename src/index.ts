a612204a5054d7efee2385fcb13ced660bb95f05
Gohér László <goher.laci@gmail.com>
2025-06-03 11:54:06 +0200

    further experimenting with mithril

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 import m from "mithril";
 
-m.render(document.body, m("h1", {class: "title"}, "This is the title"));
+
+function Counter() {
+  let count = 0;
+  return {
+    view: function() {
+      return m("div", [m("button", {onclick: function() {count += 1;}},  "Button"),
+                       m("p", "value: " + count)]);
+    }
+  }
+};
+
+const CounterPage = {
+  view: function() {
+    return m("main", [m("h1", {class: "title"}, "This is the title"),
+                      m(Counter)])
+  }
+}
+
+const WellcomePage = {
+  view: function() {
+    return m("main", [m("h1", {class: "title"}, "Wellcome!"),
+                      m("a", {href: "#!/counter"}, "Please enter")]);
+  }
+}
+
+m.route(document.body, "/wellcome", {
+  "/wellcome": WellcomePage,
+  "/counter":  CounterPage,
+});
